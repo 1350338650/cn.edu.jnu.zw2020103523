648d3ad4e35ccb6076b78f69c2396a7e5ce25224
13503 <1350338650@qq.com>
2023-03-27 11:03:07 +0800

    test 1

---
+++ app/src/main/java/com/example/cnedujnuzw2020103523/Game.java
@@ -0,0 +1,10 @@
+package com.example.cnedujnuzw2020103523;
+
+public class Game {
+    public void roll(int pins) {
+    }
+    public int score() {
+        return -2;
+    }
+
+}
--- app/src/test/java/com/example/cnedujnuzw2020103523/ExampleUnitTest.java
+++ app/src/test/java/com/example/cnedujnuzw2020103523/ExampleUnitTest.java
@@ -13,5 +13,9 @@ public class ExampleUnitTest {
     @Test
     public void testGutterGame() throws Exception {
         Game g = new Game();
+        for (int i=0; i<20; i++)
+            g.roll(0);
+        assertEquals(0, g.score());
     }
+
 }
