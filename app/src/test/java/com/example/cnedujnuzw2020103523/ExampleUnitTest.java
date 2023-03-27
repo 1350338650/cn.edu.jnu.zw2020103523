1d8c4f825853bdb3fbdf8ad480ec18994ae4ee1c
13503 <1350338650@qq.com>
2023-03-27 11:11:59 +0800

    test 2

--- app/src/test/java/com/example/cnedujnuzw2020103523/ExampleUnitTest.java
+++ app/src/test/java/com/example/cnedujnuzw2020103523/ExampleUnitTest.java
@@ -17,5 +17,12 @@ public class ExampleUnitTest {
             g.roll(0);
         assertEquals(0, g.score());
     }
+    @Test
+    public void testAllOnes() throws Exception {
+        Game g = new Game();
+        for (int i = 0; i < 20; i++)
+            g.roll(1);
+        assertEquals(20, g.score());
+    }
 
 }
